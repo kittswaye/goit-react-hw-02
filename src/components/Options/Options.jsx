408c5b53c6620171a24cc33f0c070cc76a11b0a2
kittswaye <153619900+kittswaye@users.noreply.github.com>
2024-07-25 20:20:38 +0300

    again css

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,10 +3,10 @@ import css from "./Options.module.css";
 const Options = props => {
     return (
         <>
-            <button type="button" onClick={() => props.update("good")}>Good</button>
-            <button type="button" onClick={() => props.update("neutral")}>Neutral</button>
-            <button type="button" onClick={() => props.update("bad")}>Bad</button>
-            {props.total > 0 && (<button type="button" onClick={props.reset}>Reset</button>)}
+            <button className={css.button} type="button" onClick={() => props.update("good")}>Good</button>
+            <button className={css.button} type="button" onClick={() => props.update("neutral")}>Neutral</button>
+            <button className={css.button} type="button" onClick={() => props.update("bad")}>Bad</button>
+            {props.total > 0 && (<button className={css.button} type="button" onClick={props.reset}>Reset</button>)}
         </>
     );
 };
